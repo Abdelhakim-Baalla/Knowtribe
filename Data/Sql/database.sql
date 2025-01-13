0c909650319f9252dcd45212e393c579ee2bd403
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-01-13 11:55:04 +0100

    J'ai Crée la table des categories qui est contient l'id de type int et le nom, description et logo de type string et cette table v acrée par l'engine inoodb pour sposorisé les foreign key dans le concepteur

--- Data/Sql/database.sql
+++ Data/Sql/database.sql
@@ -1,3 +1,9 @@
 CREATE DATABASE Youdemy;
 USE Youdemy;
 
+CREATE TABLE categories(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR (50),
+    description TEXT,
+    logo VARCHAR(255)
+)ENGINE=INNODB;
