ecae22cc0eb67dfb4df28e973a995da1a32b204c
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-01-14 17:36:02 +0100

    J'ai Ajouter la method magic call dans la class cours qui va inisialisez des valeur a les attributtes

--- Models/Cours.php
+++ Models/Cours.php
@@ -1,6 +1,7 @@
 <?php 
 
-    include 'Categories.php';
+    include 'Categorie.php';
+    include 'Utilisateur.php';
 
     class Cours{
         private int $id;
@@ -12,6 +13,62 @@
         private $etudiants = [];
 
         public function __construct(){}
+
+        public function __call($name, $arguments) {
+            if($name == "creeCours"){
+                if(count($arguments) == 1){
+                    $this->id = $arguments[0];
+                } 
+
+                if(count($arguments) == 2){
+                    $this->id = $arguments[0];
+                    $this->titre = $arguments[1];
+                }
+
+                if(count($arguments) == 3){
+                    $this->id = $arguments[0];
+                    $this->titre = $arguments[1];
+                    $this->description = $arguments[2];
+                }
+
+                if(count($arguments) == 4){
+                    $this->id = $arguments[0];
+                    $this->titre = $arguments[1];
+                    $this->description = $arguments[2];
+                    $this->categorie = $arguments[3];
+                }
+
+                if(count($arguments) == 5){
+                    $this->id = $arguments[0];
+                    $this->titre = $arguments[1];
+                    $this->description = $arguments[2];
+                    $this->categorie = $arguments[3];
+                    $this->enseignant = $arguments[4];
+                }
+
+                if(count($arguments) == 6){
+                    $this->id = $arguments[0];
+                    $this->titre = $arguments[1];
+                    $this->description = $arguments[2];
+                    $this->categorie = $arguments[3];
+                    $this->enseignant = $arguments[4];
+                    $this->tags = $arguments[5];
+                }
+
+                if(count($arguments) == 7){
+                    $this->id = $arguments[0];
+                    $this->titre = $arguments[1];
+                    $this->description = $arguments[2];
+                    $this->categorie = $arguments[3];
+                    $this->enseignant = $arguments[4];
+                    $this->tags = $arguments[5];
+                    $this->etudiants = $arguments[6];
+                }
+                
+
+                   
+            }
+        }
 
 
         public function setId(int $id)
