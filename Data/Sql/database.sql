da868b33a4a4e6c3c5cf9c65502baf3872ae08ec
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-01-15 17:53:57 +0100

    J'ai crée nouvelle table de utilisateur par l'ajoute de id qui est auto incrementer puis nom, prenom, email, password, phone de type string ou text puis le role id qui va presenter le role de cette utilisateur depuis la table des roles

--- Data/Sql/database.sql
+++ Data/Sql/database.sql
@@ -21,3 +21,14 @@ CREATE TABLE roles (
     description TEXT,
     logo VARCHAR(225)
 )ENGINE=INNODB;
+
+CREATE TABLE utilisateur(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(50),
+    prenom VARCHAR(50),
+    email VARCHAR(250) UNIQUE,
+    password VARCHAR(150),
+    phone VARCHAR(25),
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+)ENGINE=INNODB;
