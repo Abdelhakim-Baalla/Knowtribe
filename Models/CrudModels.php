04c43afa2dce9e3ec0bc1d43bad6bab4ed8ae57f
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-01-15 15:25:49 +0100

    J'ai preparer la requete et J'ai le executer

--- Models/CrudModels.php
+++ Models/CrudModels.php
@@ -11,34 +11,25 @@
             $this->connexion = new Connexion();
         }
 
+        // la fonctionne de la creation
         public function Create($tableName, $names)
         {
             $columns = implode(',',array_keys($names));
             $values = implode(',',array_values($names));
-            var_dump($values);
-            // if (gettype($values) == string ){
-            //     $values = "'" . $values . "'";
-            // }
             $query = "INSERT INTO " . $tableName . "(" . $columns . ")" . "VALUES(" . $values . ")";
-            var_dump($query);
+            
+            $stmt = $this->connexion->connexion()->prepare($query);
+            $stmt->execute();
         }
 
-        public function Addcolontokeys($array)
-        {
-            $newarray = [];
-            foreach($array as $key => $value)
-            {
-                $newarray[] = ':'.$key;
-    
-            }
-            $nexarray =  implode(',',$newarray);
-            return $nexarray;
-        }
+        
     }
 
-    $nn = new CrudModels();
-    $tableName = 'Utilisateur';
-    $names = ['id' => 1, 'FirstName' => 'Dino', 'LastName' => 'Crafting'];
-    $nn->Create($tableName, $names);
+
+
+    // $nn = new CrudModels();
+    // $tableName = 'roles';
+    // $names = ['nom' => "'Admin'", 'description' => "'This is admin'"];
+    // $nn->Create($tableName, $names);
 
 ?>
